61bf912ff2d709304c67a70362635b44fe0c2f97
Dmitry.Kopylov <diemon_1992@mail.ru>
2021-11-27 00:46:27 +0500

    Pause menu

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,37 @@ public class CameraFollow : MonoBehaviour
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
+    public Canvas canv;
+
+    private float cameraZoomInDelay = 2f;
+    private float timeElapsed;
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        canv = gameObject.GetComponentInChildren<Canvas>();
+        gameObject.transform.rotation = Quaternion.Euler(45, 0, 0);
     }
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPostion = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPostion;
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > cameraZoomInDelay)
+        {
+            gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(90, 0, 0), 100 * Time.deltaTime);
+            canv.transform.rotation = gameObject.transform.rotation;
+            offset = new Vector3(0, 100, 0);
+        }
+        if (timeElapsed > cameraZoomInDelay + 2)
+        {
+            gameObject.GetComponent<Camera>().orthographic = true;
+        }
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
     }
 }
