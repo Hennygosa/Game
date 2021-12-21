d430febbc7e1ccd7f32fec28f7ce903530b2b736
Dinar <72757654+Otmorozheniy@users.noreply.github.com>
2021-12-21 21:02:56 +0500

    Scenes

---
+++ Assets/Scripts/ChangeLevel2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel2 : MonoBehaviour
+{
+    public int numberOfEnemies;
+
+    private void Update()
+    {
+        numberOfEnemies = GameObject.FindGameObjectsWithTag("EnemyRanged").Length
+            + GameObject.FindGameObjectsWithTag("EnemyMelee").Length;
+        if (numberOfEnemies == 0)
+        {
+            ShowExit();
+        }
+    }
+    public void ShowExit()
+    {
+        transform.position = new Vector3(194f, 2f, 165f);
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex + 1 == SceneManager.sceneCountInBuildSettings)
+                SceneManager.LoadScene(0);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
