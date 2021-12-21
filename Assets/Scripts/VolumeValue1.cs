e6dc17d1ff8acb854ce2852e80e33d528a4c6d77
Dinar <72757654+Otmorozheniy@users.noreply.github.com>
2021-12-22 00:23:01 +0500

    Volume 2
    YEAHHHH!!!!!!!!!

--- Assets/Scripts/VolumeValue1.cs
+++ Assets/Scripts/VolumeValue1.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class VolumeValue1 : MonoBehaviour
 {
+    [SerializeField]
     private AudioSource audioSrc;
     private float musicVolume = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSrc = GetComponent<AudioSource>();
-    }
 
-    // Update is called once per frame
-    void Update()
+    private VolumeValue1 instance;
+    
+    public void SetVolume(float vol)
     {
+         musicVolume = vol;
         audioSrc.volume = musicVolume;
     }
 
-    public void SetVolume(float vol)
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        musicVolume = vol;
+        audioSrc = FindObjectOfType<MusicSource>().GetComponent<AudioSource>();
+        audioSrc.volume = musicVolume;
     }
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(this != instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        audioSrc = FindObjectOfType<MusicSource>().GetComponent<AudioSource>();
+        audioSrc.volume = musicVolume;
+    }
+    
+
 }
